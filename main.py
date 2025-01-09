925dfbcb81215b50483fb84cbcbf38f2a7a89812
Eunyoung Park <hoodyong@gmail.com>
2025-01-09 16:59:05 +0900

    update .gitignore

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 # Python version 3.6 or higher is required
 
+
 def helloworld(a):
     print(f"Hello World! {a}")
     msg = f"Hello World {a}"
     return msg
+
 
 if __name__ == "__main__":
     helloworld("Eunyoung")
