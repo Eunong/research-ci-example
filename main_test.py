5cb9ecc21c47b4b344a639332f2c6eef9f8d5eb4
Eunyoung Park <hoodyong@gmail.com>
2025-01-09 17:18:24 +0900

    apply black

--- main_test.py
+++ main_test.py
@@ -2,12 +2,14 @@ import unittest
 
 import main
 
+
 class MainTest(unittest.TestCase):
     def test_helloworld(self):
         ret = main.helloworld("Test")
         print(f"main.helloworld return : {ret}")
         print(ret)
         self.assertEqual(ret, "Hello World Test123")
+
 
 if __name__ == "__main__":
     unittest.main()
